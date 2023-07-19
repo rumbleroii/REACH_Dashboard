4afab1da1c099db0ff3a8d4bd9f29e59f2376849
Marudappa <rithik.marudappa@sap.com>
2023-07-19 16:16:33 +0530

    Calender

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import {
+  Calendar, Card, CardHeader, List, StandardListItem, Icon
+} from "@ui5/webcomponents-react";
+
+import './Home.css'
 
 const Home = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateSelect = (event) => {
+    console.log(event.detail);
+    setSelectedDate(event.detail.values[0]);
+  };
+
+  useEffect(() => {
+    const currentDate = new Date();
+    setSelectedDate(currentDate.toDateString());
+  }, []);
+
+  // Date Object
+  
   return (
-    <div>Home</div>
+    <div>
+      <div style={{ display: "flex", alignItems: "flex-start", justifyContent: "center", alignItems: "center",  height: "90vh", width: "100%" }}>
+        <div>
+          <Calendar
+            onSelectedDatesChange={handleDateSelect}
+            primaryCalendarType="Gregorian"
+          />
+        </div>
+
+        <Card
+          header={<CardHeader avatar={<Icon name="person-placeholder" />} subtitleText="Events" titleText={selectedDate}/>}
+          style={{ width: '400px', marginLeft: "100px" }}
+        >
+          {/* <List>
+            <StandardListItem description="Software Architect"> Richard Wilson</StandardListItem>
+            <StandardListItem description="Visual Designer"> Elena Petrova </StandardListItem>
+            <StandardListItem description="Quality Specialist"> John Miller </StandardListItem>
+          </List> */}
+        </Card>
+      </div>
+
+
+    </div>
   )
 }
 
