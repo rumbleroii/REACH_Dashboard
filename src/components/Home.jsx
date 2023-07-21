3ee5af289f282df8f4674e796691f3d810b27b7b
Aadhya <aadhya2003@gmail.com>
2023-07-21 15:42:33 +0530

    r,l,e buttons added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 import {
-  Calendar, Card, CardHeader, List, StandardListItem, Icon
+  Calendar,
+  Card,
+  CardHeader,
+  List,
+  StandardListItem,
+  Icon,
+  Button,
+  BorderDesign,
 } from "@ui5/webcomponents-react";
 
-import './Home.css'
+import "./Home.css";
 
 const Home = () => {
   const [selectedDate, setSelectedDate] = useState(null);
@@ -19,21 +26,59 @@ const Home = () => {
     setSelectedDate(currentDate.toDateString());
   }, []);
 
-  // Date Object
+  const buttonStyle = {
+    width: "170px", // Set the desired width of the buttons
+    height: "46px",
+    borderRadius:"10px"
+  };
+
+  const customTileStyle = {
+    width: "auto", // Set the desired width of the tiles
+    height: "auto", // Set the desired height of the tiles
+    // Add any other custom styles as needed
+  };
   
+
+  // Date Object
+
   return (
     <div>
-      <div style={{ display: "flex", alignItems: "flex-start", justifyContent: "center", alignItems: "center",  height: "90vh", width: "100%" }}>
+      <div className="button-container">
+        <div className="buttons">
+          <Button style={buttonStyle} onClick={function ka() {}}>Research</Button>
+          <div style={{ height: "16px"}} /> {/* Gap between buttons */}
+          <Button style={buttonStyle} onClick={function ka() {}}>Experience</Button>
+          <div style={{ height: "16px" }} /> {/* Gap between buttons */}
+          <Button style={buttonStyle} onClick={function ka() {}}>Learning</Button>
+        </div>
+      </div >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "90vh",
+          width: "100%",
+        }}
+      >
         <div>
           <Calendar
+            style={customTileStyle}
             onSelectedDatesChange={handleDateSelect}
             primaryCalendarType="Gregorian"
           />
         </div>
 
         <Card
-          header={<CardHeader avatar={<Icon name="person-placeholder" />} subtitleText="Events" titleText={selectedDate}/>}
-          style={{ width: '400px', marginLeft: "100px" }}
+          header={
+            <CardHeader
+              avatar={<Icon name="person-placeholder" />}
+              subtitleText="Events"
+              titleText={selectedDate}
+            />
+          }
+          style={{ width: "400px", marginLeft: "100px" }}
         >
           {/* <List>
             <StandardListItem description="Software Architect"> Richard Wilson</StandardListItem>
@@ -42,10 +87,8 @@ const Home = () => {
           </List> */}
         </Card>
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
