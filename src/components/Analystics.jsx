a19b828d4c5edb04f9db888cbcf65dc5f0252427
Aadhya <aadhya2003@gmail.com>
2023-07-25 15:17:06 +0530

    routing to event details

--- src/components/Analystics.jsx
+++ src/components/Analystics.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-
-import { AnalyticalTable, ProgressIndicator, FlexBox, Button, Text, ObjectStatus } from '@ui5/webcomponents-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AnalyticalTable, ProgressIndicator, ObjectStatus,FlexBox,Button } from '@ui5/webcomponents-react';
 
 // Status
 const statusObject = {
@@ -8,62 +8,72 @@ const statusObject = {
   "Warning": "Delayed",
   "Information": "Completed",
   "Error": "Cancelled",
-}
+};
 
-const Analystics = (tableData) => {
+const Analystics = ({ tableData }) => {
   return (
     <AnalyticalTable
-    columns={[
-      {
-        Header: 'Event Title',
-        accessor: 'eventName',
-        minWidth: 200
-      },
-      {
-        Header: 'Deadline',
-        accessor: 'deadline',
-      },
-      {
-        Header: 'Status',
-        accessor: 'status',
-        Cell: (instance) => {        
-          return <ObjectStatus showDefaultIcon state={instance.cell.value}>{statusObject[instance.cell.value]}</ObjectStatus>
-        }
-      },
-      {
-        Header: 'Category',
-        accessor: 'pillar',
-      },
-      {
-        Header: 'Progress',
-        accessor: 'progress',
-        width:300,
-        Cell: (instance) => { 
-            return <ProgressIndicator value={instance.cell.value}/> 
-        }
-      },
-      // {
-      //   Header: "Edit",
-      //   Cell: (instance) => {
-      //       const { cell, row, webComponentsReactProperties } = instance;
-      //       // disable buttons if overlay is active to prevent focus
-      //       const isOverlay = webComponentsReactProperties.showOverlay;
-      //       // console.log('This is your row data', row.original);
-      //       return (
-      //           <FlexBox>
-      //               <Button icon="edit" disabled={isOverlay} />
-      //           </FlexBox>
-      //           );
-      //       }
-      //   }
-    ]}
-    scaleWidthMode='Smart'
-    data={tableData.data}
-    filterable
-    rowHeight={44}
-    withRowHighlight
+      columns={[
+        {
+          Header: 'Event Title',
+          accessor: 'eventName',
+          minWidth: 200,
+          Cell: (instance) => (
+            <Link to={`/event-details/${instance.row.original.id}`}> {/* Replace "/event-details" with the actual URL of the event details page */}
+              {instance.cell.value}
+            </Link>
+          ),
+        },
+        {
+          Header: 'Deadline',
+          accessor: 'deadline',
+        },
+        {
+          Header: 'Status',
+          accessor: 'status',
+          Cell: (instance) => {
+            return (
+              <ObjectStatus showDefaultIcon state={instance.cell.value}>
+                {statusObject[instance.cell.value]}
+              </ObjectStatus>
+            );
+          },
+        },
+        {
+          Header: 'Category',
+          accessor: 'pillar',
+        },
+        {
+          Header: 'Progress',
+          accessor: 'progress',
+          width: 300,
+          Cell: (instance) => {
+            return <ProgressIndicator value={instance.cell.value} />;
+          },
+        },
+        {
+          Header: 'Edit',
+          Cell: (instance) => {
+            const { cell, row, webComponentsReactProperties } = instance;
+            // disable buttons if overlay is active to prevent focus
+            const isOverlay = webComponentsReactProperties.showOverlay;
+            // console.log('This is your row data', row.original);
+            return (
+              <FlexBox>
+                <Link to={`/edit-event/${row.original.id}`}> {/* Replace "/edit-event" with the actual URL of the edit event page */}
+                  <Button icon="edit" disabled={isOverlay} />
+                </Link>
+              </FlexBox>
+            );
+          },
+        },
+      ]}
+      data={tableData.data}
+      filterable
+      rowHeight={44}
+      withRowHighlight
     />
-  )
-}
+  );
+};
 
-export default Analystics
+export default Analystics;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import {
   FlexBox,
   FlexBoxAlignItems,
   FlexBoxJustifyContent,
-  Text
+  Text,
 } from "@ui5/webcomponents-react";
 
 import { Link } from "react-router-dom"; // Step 1: Import Link
@@ -15,7 +15,7 @@ import { Link } from "react-router-dom"; // Step 1: Import Link
 import Analystics from "./Analystics";
 
 import "./Home.css";
-import data from './data.json';
+import data from "./data.json";
 import { sapUiLargeMarginBottom } from "@ui5/webcomponents-react-base/dist/styling/spacing";
 
 const Home = () => {
@@ -26,7 +26,7 @@ const Home = () => {
   // useEffect(() => {
   //   const currentDate = new Date().toDateString();
   //   const dateObj = currentDate.split(' ');
-    
+
   //   const modifiedDate = dateObj[2] + " " + dateObj[1] + ", " + dateObj[3];
   //   console.log(modifiedDate);
   //   setSelectedDate(modifiedDate);
@@ -34,110 +34,158 @@ const Home = () => {
 
   const handleDateSelect = (event) => {
     setSelectedDate(event.detail.values[0]);
-    setData(data.filter((item) => {
-      if(emphasizedButton === null) return item.startDate === event.detail.values[0];
-      else return item.startDate === event.detail.values[0] && item.pillar === emphasizedButton;
-    }))
+    setData(
+      data.filter((item) => {
+        if (emphasizedButton === null)
+          return item.startDate === event.detail.values[0];
+        else
+          return (
+            item.startDate === event.detail.values[0] &&
+            item.pillar === emphasizedButton
+          );
+      })
+    );
   };
 
   const handleAllEvent = () => {
     setData(data);
     setEmphasizedButton(null);
     setSelectedDate("All Events");
-  }
+  };
   const handleButtonClick = (buttonName) => {
     setEmphasizedButton((prevButton) => {
-      if(prevButton === buttonName && selectedDate === "All Events") {
+      if (prevButton === buttonName && selectedDate === "All Events") {
         setData(data);
         return null;
       }
-      if(prevButton === buttonName) {
-        setData(data.filter((item) => item.startDate === selectedDate ))
+      if (prevButton === buttonName) {
+        setData(data.filter((item) => item.startDate === selectedDate));
         return null;
-      } else if(selectedDate === "All Events") {
-        setData(data.filter((item) => item.pillar === buttonName))
+      } else if (selectedDate === "All Events") {
+        setData(data.filter((item) => item.pillar === buttonName));
         return buttonName;
       } else {
-        setData(data.filter((item) => item.pillar === buttonName && item.startDate === selectedDate ))
+        setData(
+          data.filter(
+            (item) =>
+              item.pillar === buttonName && item.startDate === selectedDate
+          )
+        );
         return buttonName;
       }
     });
   };
 
-    // Function to add a new event to the state
-    const addEvent = (eventData) => {
-      setData([...updatedData, eventData]);
-    };
+  // Function to add a new event to the state
+  const addEvent = (eventData) => {
+    setData([...updatedData, eventData]);
+  };
 
   const buttonStyle = {
     width: "170px",
     height: "46px",
     fontSize: "15px",
-    marginLeft:"50px",
-    marginRight:"50px",
-    border: "1.5px solid", 
+    marginLeft: "50px",
+    marginRight: "50px",
+    border: "1.5px solid",
     fontWeight: emphasizedButton === "Research" ? "bold" : "normal",
   };
 
   return (
     <>
-    <Card style={{ height: '100px'}}>
-      <FlexBox alignItems={FlexBoxAlignItems.Center} justifyContent={FlexBoxJustifyContent.Center} style={{paddingTop:'25px'}}>
-        <Button
-          style={buttonStyle}
-          onClick={() =>  handleButtonClick("Research")}
-          design={emphasizedButton === "Research" ? "Emphasized" : "Default"}
+      <Card style={{ height: "100px" }}>
+        <FlexBox
+          alignItems={FlexBoxAlignItems.Center}
+          justifyContent={FlexBoxJustifyContent.Center}
+          style={{ paddingTop: "25px" }}
         >
-          {<b>Research</b>}
-        </Button>
-        <Button
-          style={buttonStyle}
-          onClick={() => handleButtonClick("Learning")}
-          design={emphasizedButton === "Learning" ? "Emphasized" : "Default"}
-        >
-          {<b>Learning</b>}
-        </Button>
-        <Button
-          style={buttonStyle}
-          onClick={() => handleButtonClick("Experience")}
-          design={emphasizedButton === "Experience" ? "Emphasized" : "Default"}
-        >
-          {<b>Experience</b>}
-        </Button>              
-      </FlexBox>
-    </Card>    
-    <FlexBox alignItems={FlexBoxAlignItems.Center} justifyContent={FlexBoxJustifyContent.Center} style={{ height: "60vh", marginBottom:"80px", marginTop:"35px", marginLeft:"10px", marginRight:"10px"}}>
-      <FlexBox alignItems={FlexBoxAlignItems.Center} direction="Column">
-        <Text style={{fontSize:"20px", margin:"10px"}}> Event Calender </Text>
-        <Calendar 
-          style={{boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'}} 
-          onSelectedDatesChange={handleDateSelect} 
-          primaryCalendarType="Gregorian"
-          children={(instance) => {
-            console.log(instance);
-          }}
-        >
-          lols
-        </Calendar>
-      </FlexBox>
-        
-      <FlexBox alignItems={FlexBoxAlignItems.Center}  style={{ width: "60%", marginLeft:"50px", display:"block" }}>
-        <Link to="/create-event">
-          <Button design="Emphasized" style={{float:"right", marginBottom:"12px"}}>
-            <b>Create Event</b>
+          <Button
+            style={buttonStyle}
+            onClick={() => handleButtonClick("Research")}
+            design={emphasizedButton === "Research" ? "Emphasized" : "Default"}
+          >
+            {<b>Research</b>}
           </Button>
-        </Link>
-        <Button onClick={handleAllEvent} style={{marginBottom:"12px"}}>
-          <b>All Events</b>
-        </Button>
-        <Card  header={<CardHeader status={`${updatedData.length} events`} subtitleText={emphasizedButton === null ? "All Categories" : emphasizedButton} titleText={selectedDate}/>}>
-          <Analystics data={updatedData} />
-        </Card>
+          <Button
+            style={buttonStyle}
+            onClick={() => handleButtonClick("Learning")}
+            design={emphasizedButton === "Learning" ? "Emphasized" : "Default"}
+          >
+            {<b>Learning</b>}
+          </Button>
+          <Button
+            style={buttonStyle}
+            onClick={() => handleButtonClick("Experience")}
+            design={
+              emphasizedButton === "Experience" ? "Emphasized" : "Default"
+            }
+          >
+            {<b>Experience</b>}
+          </Button>
+        </FlexBox>
+      </Card>
+      <FlexBox
+        alignItems={FlexBoxAlignItems.Center}
+        justifyContent={FlexBoxJustifyContent.Center}
+        style={{
+          height: "60vh",
+          marginBottom: "80px",
+          marginTop: "35px",
+          marginLeft: "10px",
+          marginRight: "10px",
+        }}
+      >
+        <FlexBox alignItems={FlexBoxAlignItems.Center} direction="Column">
+          <Text style={{ fontSize: "20px", margin: "10px" }}>
+            {" "}
+            Event Calender{" "}
+          </Text>
+          <Calendar
+            style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" }}
+            onSelectedDatesChange={handleDateSelect}
+            primaryCalendarType="Gregorian"
+            children={(instance) => {
+              console.log(instance);
+            }}
+          >
+            lols
+          </Calendar>
+        </FlexBox>
+
+        <FlexBox
+          alignItems={FlexBoxAlignItems.Center}
+          style={{ width: "60%", marginLeft: "50px", display: "block" }}
+        >
+          <Link to="/create-event">
+            <Button
+              design="Emphasized"
+              style={{ float: "right", marginBottom: "12px" }}
+            >
+              <b>Create Event</b>
+            </Button>
+          </Link>
+          <Button onClick={handleAllEvent} style={{ marginBottom: "12px" }}>
+            <b>All Events</b>
+          </Button>
+          <Card
+            header={
+              <CardHeader
+                status={`${updatedData.length} events`}
+                subtitleText={
+                  emphasizedButton === null
+                    ? "All Categories"
+                    : emphasizedButton
+                }
+                titleText={selectedDate}
+              />
+            }
+          >
+            <Analystics tableData={{ data: updatedData }} /> {/* Pass updatedData as tableData prop */}
+          </Card>
+        </FlexBox>
       </FlexBox>
-    </FlexBox>
     </>
   );
 };
 
 export default Home;
-
