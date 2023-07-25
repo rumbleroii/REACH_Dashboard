ca4076acc682232827aa60aa133f466272132335
Gandeti <vishnu.priya.gandeti@sap.com>
2023-07-25 14:39:03 +0530

    Mou navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,12 +6,23 @@ import editIcon from "@ui5/webcomponents-icons/dist/edit.js";
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
+  const handleTextClick = () => {
+    // Define the URL or path to the page you want to navigate to
+    const targetPageUrl = '/Mou';
+    window.location.href = targetPageUrl;
+  };
     return (
     <ShellBar
         logo={<img alt="SAP Logo" src="https://sap.github.io/ui5-webcomponents/assets/images/sap-logo-svg.svg"/>}
         primaryTitle="REACH Dashboard"
       >
-      <ShellBarItem text="mou"/>
+      <span
+        slot="profile"
+        style={{ cursor: 'pointer', textDecoration: 'underline' }}
+        onClick={handleTextClick}
+      >
+        Mou's
+      </span>
     </ShellBar>
   );
 }
